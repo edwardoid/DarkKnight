78920cda99369d150b42f81f9a09071b34dc0f94
Edward Sarkisyan <edward.sarkisyan@gmail.com>
2013-05-26 14:44:30 +0400

    Interface of calculation data changed. Now if will use color while calculations.

--- Framework/CalculationData.h
+++ Framework/CalculationData.h
@@ -6,13 +6,14 @@
 #include <AbstractCalculationData.h>
 #include <CalculationResultForGame.h>
 #include <ConceptCalculationsCache.h>
+#include <CEPiece.h>
 
 class FRAMEWORK_EXPORT CalculationData: public CGSQL_NS::AbstractCalculationData, public FrameworkUser
 {
 public:
 	CalculationData(const pgn::Game* game = NULL);
 	void addGame(const pgn::Game* game);
-	virtual bool calculate(const char* concept, const char* requieredValue) const;
+	virtual bool calculate(const char* concept, const char* requieredValue, ChEngn::piece_color color) const;
 	virtual ~CalculationData();
 	inline void clear()  { m_conceptToResult.clear(); };
 	inline bool isEmpty() const { return m_conceptToResult.isEmpty(); }
